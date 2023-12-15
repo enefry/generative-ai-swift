a3ccb8fbfad0059027e2f87914e4a5369b7d0b5b
陈 <2153953+enefry@users.noreply.github.com>
2023-12-15 11:32:50 +0800

    Update GenerativeAISwift.swift
    public base url

--- Sources/GoogleAI/GenerativeAISwift.swift
+++ Sources/GoogleAI/GenerativeAISwift.swift
@@ -22,5 +22,5 @@ import Foundation
 public enum GenerativeAISwift {
   /// String value of the SDK version
   public static let version = "0.5.2"
-  static let baseURL = "https://generativelanguage.googleapis.com"
+  public static let baseURL = "https://generativelanguage.googleapis.com"
 }
